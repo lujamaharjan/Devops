32b79321807a00db10b3ffc0d553973fbc5e99a4
sachin.maharjan <lujamaharjan7@gmail.com>
2025-06-10 15:55:46 +0545

    change in url api

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://api:5001/todos"; // Update port if needed
+const apiUrl = "https://192.168.56.108:5001/todos"; // Update port if needed
 
 const todoList = document.getElementById("todo-list");
 const todoForm = document.getElementById("todo-form");
